e2d4bb86fdec09e13c0fc3c4c4846ec2234ad069
mkg203 <kailashguru129@gmail.com>
2026-02-05 20:25:39 +0530

    improved setup process

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+git checkout long_context_exp
 
 apt-get update
 apt-get install libnuma1
@@ -6,5 +6,10 @@ apt-get install libnuma1
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-uv venv -p 3.12
-uv pip install -r requirements.txt
+uv sync
+
+if [[ -n $HF_TOKEN ]]; then
+  hf auth login --token $HF_TOKEN
+else
+  echo "hugging face cli not initialized"
+fi
