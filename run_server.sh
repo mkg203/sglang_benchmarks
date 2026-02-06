e564eafbbc156c4091d5072dd39d0a48d703370a
mkg203 <kailashguru129@gmail.com>
2026-02-06 14:33:28 +0530

    automation fix

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,5 @@
-uv run -m sglang.launch_server \
-  --model-path meta-llama/Llama-3.1-8B \
+python -m sglang.launch_server \
+  --model-path meta-llama/Llama-3.1-8B-Instruct \
   --port 30000 \
   --enable-metrics \
   # --enable-hierarchical-cache
