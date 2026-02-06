1d659f5230e2262287739bcce754d288d107c9d5
mkg203 <kailashguru129@gmail.com>
2026-02-06 15:01:52 +0530

    final fix, automation works now

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -2,14 +2,16 @@ source .venv/bin/activate
 
 for i in workload_long_ctx/*;
 do 
+  > server.log
   stdbuf -oL bash run_server.sh > >(tee server.log) &
   SERVER_PID=$!
 
-  tail -f server.log --pid=$SERVER_PID | rg -m 1 "The server is fired up and ready to roll!"
+  tail -f server.log --pid=$SERVER_PID | grep -m 1 "/generate"
+  sleep 5
 
   echo "--- Server is ready. Starting Benchmarks ---"
 
-  python benchmarks.py $i --output "results/$(echo $i  | awk -F "_turns" '{print $1}')"
+  python benchmarks.py $i --output "results/$(echo $i  | awk -F "_turns" '{print $1}')" > benchmark_progress.log
 
   echo "--- Benchmarks finished. Shutting down server ---"
   
